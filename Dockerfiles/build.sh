2fdf93e64095eb0222e4a9b687582844104ac2e0
Jordi Esteban <jest10820@gmail.com>
2017-10-17 19:54:02 +0200

    Basic vue.js container

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -5,3 +5,4 @@ currdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t="jest10820/sm-pgsql" "$currdir"/postgresql
 docker build -t="jest10820/sm-pgadmin" "$currdir"/pgadmin
 docker build -t="jest10820/python" "$currdir"/python
+docker build -t="jest10820/vue" "$currdir"/vue
