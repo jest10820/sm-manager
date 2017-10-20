ba07a7c9d658746873ea2e9de54d45da1b3de6d3
Jordi Esteban <jest10820@gmail.com>
2017-10-20 13:32:18 +0200

    Update build script with new services

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -6,3 +6,5 @@ docker build -t="jest10820/sm-pgsql" "$currdir"/postgresql
 docker build -t="jest10820/sm-pgadmin" "$currdir"/pgadmin
 docker build -t="jest10820/python" "$currdir"/python
 docker build -t="jest10820/vue" "$currdir"/vue
+docker build -t="jest10820/vue-frontend" "$currdir"/frontend
+docker build -t="jest10820/nginx" "$currdir"/nginx
