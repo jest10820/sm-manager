89e1482b51136c23d00d9ac522a18a4c0f9e887f
Jordi Esteban <jesteban@iris.cat>
2017-10-26 19:44:07 +0200

    Build backend images

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -8,3 +8,5 @@ docker build -t="jest10820/python" "$currdir"/python
 docker build -t="jest10820/vue" "$currdir"/vue
 docker build -t="jest10820/vue-frontend" "$currdir"/vue-frontend
 docker build -t="jest10820/nginx" "$currdir"/nginx
+docker build -t="jest10820/sm-backend" "$currdir"/backend
+docker build -t="jest10820/sm-backend-dev" --file "$currdir"/backend/Dockerfile-dev "$currdir"/backend
